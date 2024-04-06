23ac3f7248ec4c6fa68a71762f64ce63e600b727
Ramjay Romorosa <ramjayromorosa@yahoo.com>
2024-04-06 23:28:42 +0800

    Εισαγωγή Haskell

---
+++ HaskellEna.hs
@@ -0,0 +1,46 @@
+import Data.List
+import System.IO
+
+-- Σχόλιο
+
+{-
+Polygrammés
+-}
+
+-- Data Types
+
+-- Int -2^63 2^63
+-- maxInt = maxBound :: Int 
+minInt = minBound :: Int 
+
+-- Integer
+
+-- Float
+
+-- Double
+bigFloat = 3.9999999 + 0.000005
+
+-- Bool
+
+-- Char
+
+-- Tuple
+
+-- Constant
+always5 :: Int
+always5 = 5
+
+-- Math Functions
+
+--sumOfNums = sum[1..1000]
+addEx = 5 + 4
+subEx = 5 - 4
+mulEx = 5 * 4
+
+divEx = 5 / 4
+
+-- Modulus
+modEx = mod 5 4
+-- Prefix
+modEx2 = 5 `mod` 4
+
